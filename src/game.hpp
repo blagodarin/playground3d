97188f992b487c99e555557d9b89775a9f49bd7a
Sergey Blagodarin <s.blagodarin@gmail.com>
2021-03-08 15:37:17 +0300

    Initial commit of Yttrium example

---
+++ src/game.hpp
@@ -0,0 +1,38 @@
+// This file is part of Playground3D.
+// Copyright (C) Sergei Blagodarin.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <chrono>
+#include <memory>
+
+namespace Yt
+{
+	class Gui;
+	class RenderPass;
+	struct RenderReport;
+	class ResourceLoader;
+	class Window;
+	class Vector2;
+}
+
+class GameState;
+class MinimapCanvas;
+class WorldCanvas;
+
+class Game
+{
+public:
+	Game(Yt::ResourceLoader&, Yt::Gui&);
+	~Game() noexcept;
+
+	void draw_debug_graphics(Yt::RenderPass&, const Yt::Vector2& cursor, const Yt::RenderReport&);
+	void toggle_debug_text() noexcept;
+	void update(const Yt::Window&, std::chrono::milliseconds);
+
+private:
+	const std::unique_ptr<GameState> _state;
+	const std::unique_ptr<WorldCanvas> _world;
+	const std::unique_ptr<MinimapCanvas> _minimap;
+};
